a6b0d3634311fbf493448820794664a15eedf129
Justin Punzalan <justpunzz@gmail.com>
2020-11-30 15:56:49 -0800

    Added Intercom

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// INIT
+window.intercomSettings = {
+  app_id: "ltjwars5",
+};
+intercomInit();
+// END INIT
+
 function showSideNav() {
   const sideNav = document.getElementById("side-nav");
   const sideNavContent = document.getElementById("side-nav-content");
@@ -68,4 +75,36 @@ function enableScroll() {
   window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
   window.removeEventListener("touchmove", preventDefault, wheelOpt);
   window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+function intercomInit() {
+  const w = window;
+  const ic = w.Intercom;
+  if (typeof ic === "function") {
+    ic("reattach_activator");
+    ic("update", w.intercomSettings);
+  } else {
+    const d = document;
+    const i = function () {
+      i.c(arguments);
+    };
+    i.q = [];
+    i.c = function (args) {
+      i.q.push(args);
+    };
+    w.Intercom = i;
+    const l = function () {
+      const s = d.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.intercom.io/widget/ltjwars5";
+      const x = d.getElementsByTagName("script")[0];
+      x.parentNode.insertBefore(s, x);
+    };
+    if (w.attachEvent) {
+      w.attachEvent("onload", l);
+    } else {
+      w.addEventListener("load", l, false);
+    }
+  }
 }
